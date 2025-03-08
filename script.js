bf0414dc7f851b7c105a27209f46bd23152761ba
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-08 09:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	let trimmedStr = s.trim();
+    let spaceIndex = trimmedStr.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    return spaceIndex === -1 ? trimmedStr : trimmedStr.substring(0, spaceIndex);
 }
 
 // Do not change the code below
